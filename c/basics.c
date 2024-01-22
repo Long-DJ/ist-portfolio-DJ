627349848668bef1a2dd85a6e216c574770deae5
DJ <2027ananthanarayanan.dhiraj@fbcoe.org>
2024-01-22 15:41:22 -0500

    I love your number

---
+++ c/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+	
+	printf("I LIKE BIG THINGS\n");
+	int num;
+	printf("What is your NUMBER BABY ???\n");
+	scanf("%i", &num);
+	printf("You have a hot number which is %i\n", num);
+	return 0;
+}
